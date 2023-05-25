cfe3066eeae699b2d7a258a48ce9a826840c7d01
Сергей В. Фокин <@office.local>
2023-05-25 09:04:46 +0300

    Добавьте файлы проекта.

---
+++ Depoller.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Stend_3
+{
+    public enum Tag
+    {
+        Reg0, 
+        Reg1
+    }
+
+    /// <summary>
+    /// Готов принимать сырые регистры по опросной модели и преобразовывать
+    /// их в теги с уведомлением об изменениях по событийной модели
+    /// </summary>
+    /// <remarks>
+    /// Для простоты привязки сделан динамическим объектом. Свойства = теги.
+    /// </remarks>
+    public class Depoller : DynamicObject, INotifyPropertyChanged
+    {
+        // Сюда новые и старые значения тегов
+        IDictionary<Tag, Object> oldValues, newValues;
+
+        // Сюда названия тегов, изменившихся на данном проходе
+        List<Tag> dirtyTags = new List<Tag>();
+
+        // Через это событие другие объекты узнают об изменениях
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        Dispatcher dispatcher;
+
+        public Depoller(Dispatcher d)
+        {
+            dispatcher = d;
+            oldValues = new Dictionary<Tag, Object>();
+            newValues = new Dictionary<Tag, Object>();
+        }
+
+        /// <summary>
+        /// Разбирает Modbus-регистры на теги
+        /// </summary>
+        /// <param name="data">Modbus-регистры как есть</param>
+        public void Input(ushort[] data)
+        {
+            // Парсим
+            newValues[Tag.Reg0] = (double)data[0];
+            newValues[Tag.Reg1] = (double)data[1];
+            // Составляем список изменившихся
+            lock (dirtyTags)
+            {
+                // Новый список
+                dirtyTags.Clear();
+
+                foreach (var pair in newValues)
+                {
+                    Object val = null;
+                    // Если тег записывается впервые или если он изменился
+                    if (!oldValues.TryGetValue(pair.Key, out val) || !pair.Value.Equals(val))
+                    {
+                        oldValues[pair.Key] = pair.Value; // Запоминаем на следующий раз
+                        dirtyTags.Add(pair.Key); // Добавляем в список изменённый на этот раз
+                    }
+                }
+            }
+            if (PropertyChanged != null) // Если есть с кем поделиться этой радостной вестью
+            {
+                // В потоке пользовательского интерфейса
+                dispatcher.BeginInvoke(new Action(delegate()
+                {
+                    lock (dirtyTags)
+                    {
+                        // Сообщить о каждом изменённом теге
+                        foreach (var tag in dirtyTags)
+                        {
+                            PropertyChanged(this, new PropertyChangedEventArgs(tag.ToString()));
+                        }
+                    }
+                }));
+            }
+        }
+
+        /// <summary>
+        /// Доступ к тегам как к свойствам объекта
+        /// </summary>
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return oldValues.TryGetValue((Tag)Enum.Parse(typeof(Tag), binder.Name), out result);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿//проект работает в связке с test_3.pro
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Modbus.Device;
+using System.Net.Sockets;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Windows.Threading;
+using System.Threading.Tasks;
+
+
+namespace Stend_3
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	/// 
+
+	public partial class MainWindow : Window
+	{
+		private double a = 50;
+		private ushort b = 50;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			TagSource = new Depoller(Dispatcher);
+		}
+
+		ModbusIpMaster mbMaster;
+		Task masterLoop;
+
+		private void Update()
+		{
+			while (true)
+			{
+				TagSource.Input(mbMaster.ReadHoldingRegisters(1, 0, 2));  //последнее чисол - количество считываемых регистров
+			}
+		}
+
+		private void Grid_Loaded(object sender, RoutedEventArgs e)
+		{
+			mbMaster = ModbusIpMaster.CreateIp(new TcpClient("192.168.0.30", 502));
+			masterLoop = Task.Factory.StartNew(new Action(Update));
+		}
+
+		public Depoller TagSource
+		{
+			get;
+			private set;
+		}
+		//----------------------------------------------------------------------------------------
+		private void Connect_but_Click(object sender, RoutedEventArgs e)
+		{
+			mbMaster.WriteSingleRegister(1, 0, 0);
+		}
+		
+		private void Pusk_Click(object sender, RoutedEventArgs e)
+		{
+			a = MySlider.Value;
+			b = (ushort)a;
+			mbMaster.WriteSingleRegister(1, 0, b);
+		}
+
+		private void progressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+
+		}
+		
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			a = 50;
+		}
+	}
+}
